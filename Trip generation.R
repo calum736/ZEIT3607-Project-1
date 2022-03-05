88447066d4cc5d3e981094b8d6513a1810be7906
calum736 <36124506+calum736@users.noreply.github.com>
2022-03-05 23:17:14 +1100

    Setting up variables for model parameters
    Setting up variables for model parameters using assignment sheet data.

--- Trip generation.R
+++ Trip generation.R
@@ -1,19 +1,41 @@
-#---- Packages ----
+#---- PARAMETERS ----
+FT_STAFF <- 0.80
+PT_STAFF <- 0.20
+
+PG_STUDENT <- 0.60
+UG_STUDENT <- 0.40
+
+STUDENT_CNT <- 5000
+STAFF_CNT <- 700
+
+BASE_YEAR <- 2020
+TARGET_YEAR <- 2040
+TIME_GROWTH <- 0.15 # travel times expected to increase 15% in target year
+
+#---- PACKAGES ----
 library(tidyverse)
 
-#---- Read data ----
-directory <- getwd()
+#---- READ CSV DATA ----
 student_data <- read.csv(file = 'data/1_Students.csv')
 staff_data <- read.csv(file = 'data/1_Staff.csv')
 
 student_IDs <- read.csv(file = 'data/Group_3_StudentIDs.csv')
 staff_IDs <- read.csv(file = 'data/Group_3_StaffIDs.csv')
 
-#---- Pull sample subset ----
+#---- GET COUNTS ----
+ADFA_STUDENT_CNT <- n_distinct(student_data$ID)
+ADFA_STAFF_CNT <- n_distinct(staff_data$ID)
+
+#---- EXTRACT GROUP 3 SAMPLE ----
 student_sample <- filter(student_data, student_data$ID %in% student_IDs$ID)
 staff_sample <- filter(staff_data, staff_data$ID %in% staff_IDs$ID)
 
-# Write samlple sets to CSV
+
+
+
+
+
+# Write sample set to CSV for future reference
 write.csv(student_sample, "Group_3_Student_Data.csv")
 write.csv(staff_sample, "Group_3_Staff_Data.csv")
 
@@ -23,13 +45,3 @@ student_sample %>%
   geom_bar(aes(Entrance, fill = Day)) +
   facet_wrap("Day")
 
-
-
-z1 <- student_sample %>% 
-      filter(Day == "Wednesday") %>% 
-      count(Entrance)
-z2 <- student_sample %>% 
-      filter(Day == "Wednesday") %>% 
-      count(Exit)
-#z3[ <- z1[z2, Entrance - i.Exit]
-      
