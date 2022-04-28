7c97413174c09952a958635263e1fae94c7bf9b3
calum736 <36124506+calum736@users.noreply.github.com>
2022-04-28 23:48:35 +1000

    Updated script to v2

--- Data Analysis v2.R
+++ Data Analysis v2.R
@@ -1,136 +1,248 @@
-#- Packages ----
-library(tidyverse)
 
-#- Read data ----
+#---- PACKAGES ----
+library(tidyverse)
+library(janitor)
+
+#---- UTILITY FUNCTIONS -----
+x_prod_list <- function(A, B) {
+  x_list <- vector(mode = "numeric", len = 0) # should mode be "list"?
+  for (i in 1:length(A)) {
+    for (j in 1:length(B)) {
+      key <- paste(names(A)[[i]], names(B)[[j]], sep = "_")
+      value <- A[[i]] * B[[j]]
+      print(key)
+      print(value)
+      x_list[[key]] <- value
+    }
+  }
+  return(x_list)
+}
+
+#----------------------------------------------#
+#--- LOAD SAMPLE DATA -------------------------#
+#----------------------------------------------#
+
+#- Read student and staff data from master file
 student_master <- read.csv(file = "data/1_Students.csv")
 staff_master <- read.csv(file = "data/1_Staff.csv")
 
 student_IDs <- read.csv(file = "data/Group_3_StudentIDs.csv")
 staff_IDs <- read.csv(file = "data/Group_3_StaffIDs.csv")
 
-#- Pull group 3 data -----
-Data_Student <- filter(student_master, ID %in% student_IDs$ID)
-Data_Staff <- filter(staff_master, ID %in% staff_IDs$ID)
+#----------------------------------------------#
+#--- 1. TRIP GENERATION -----------------------#
+#----------------------------------------------#
 
-#- Trip generation ----
-Data_Student_Weekly <- 
-  Data_Student %>% 
-  group_by(ID) %>% 
-  summarise(WeeklyTrips=n(), School=first(School), Career=first(Career), Gender=first(Gender))
-write.csv(Data_Student_Weekly, 'output data/Data_Student_weekly_av_trips.csv')
+#- Pull out a subset of data from the sample list
+student_sample <-
+  filter(student_master, student_master$ID %in% student_IDs$ID)
+staff_sample <-
+  filter(staff_master, staff_master$ID %in% staff_IDs$ID)
 
-Data_Staff_Weekly <- 
-  Data_Staff %>% 
-  group_by(ID) %>% 
-  summarise(WeeklyTrips=n(), School=first(School), Level=first(Level), FulltimeParttime=first(FulltimeParttime))
-write.csv(Data_Staff_Weekly, 'output data/Data_Student_weekly_av_trips.csv')
-# Data_1 <-
-#   Data_Student %>% 
-#   group_by(ID, School, Career) %>% 
-#   summarise(n = n()) 
-#   
+n_students <- n_distinct(student_sample$ID)
+n_staff <- n_distinct(staff_sample$ID)
 
-# Student_Weekly_Model <- lm(WeeklyTrips~School+Career+Gender, data = Data_Student_Weekly)
-# summary(Student_Weekly_Model)
-# par(mfrow = c(2, 2))
-# plot(Student_Weekly_Model)
-# 
-# #plot(Student_Weekly_Model)
-# 
-# #http://www.sthda.com/english/articles/40-regression-analysis/163-regression-with-categorical-variables-dummy-coding-essentials-in-r/
-# 
-# #- outsample validation ----
-# Data_Student_Weekly_Train <- 
-#   Data_Student_Weekly %>% 
-#   sample_frac(0.8)
-# 
-# Data_Student_Weekly_Test <- 
-#   Data_Student_Weekly %>% 
-#   filter(!(ID %in% Data_Student_Weekly_Train$ID))
-# 
-# Model_Student <- lm(WeeklyTrips~School+Career+Gender, data=Data_Student_Weekly_Train)
-# summary(Model_Student)
-# 
-# Predicted_trips <- predict(Model_Student, newdata = Data_Student_Weekly_Test)
-# 
-# Data_Student_Weekly_Test <- 
-#   Data_Student_Weekly_Test %>% 
-#   cbind(Predicted_trips)
-# 
-# 
-# Data_Student_Weekly_Test %>% 
-#   ggplot()+
-#   geom_point(aes(x=Predicted_trips, y=WeeklyTrips), position = "jitter")
-# 
-# 
-# Data_Student_Weekly_Test %>% 
-#   mutate(Difference = (Predicted_trips - WeeklyTrips)^2) %>% 
-#   group_by() %>% 
-#   summarise(RMSE = (sum(Difference)/nrow(Data_Student_Weekly_Test))^0.5)
+#- Average student weekly trips by school, pg/ug and gender
+av_trips_student <- student_sample %>%
+  group_by(ID, School, Career) %>%
+  summarise(WeeklyTrips = n()) %>%
+  group_by(School, Career) %>%
+  summarise("AverageTrips" = mean(WeeklyTrips)) %>%
+  mutate(ID = paste(School, Career, sep = "_"),
+         .before = School)
+
+#- Average staff weekly trips by School, pt/ft and role
+av_trips_staff <- staff_sample %>%
+  group_by(ID, School, FulltimeParttime) %>%
+  summarise(WeeklyTrips = n()) %>%
+  group_by(School, FulltimeParttime) %>%
+  summarise("AverageTrips" = mean(WeeklyTrips)) %>%
+  mutate(ID = paste(School, FulltimeParttime, sep = "_"),
+         .before = School)
 
 
-Staff_Weekly_Model <- lm(WeeklyTrips~School+Level+FulltimeParttime, data = Data_Staff_Weekly)
-anova(Staff_Weekly_Model)
+#- Define the parameters for the new city campus (as given in the brief)
+N_STUDENT <- 5000
+N_STAFF <- 700
+CAREER <- c("PGRD" = 0.60,
+            "UGRD" = 0.40)
+GENDER <- c("M" = 0.50,
+            "F" = 0.50)
+POSITION_TITLE <- c(
+  "admin" = 0.10,
+  "executive" = 0.10,
+  "director" = 0.05,
+  "professor" = 0.05,
+  "associate_professor" = 0.05,
+  "senior_lecturer" = 0.15,
+  "lecturer" = 0.15,
+  "research_fellow" = 0.25,
+  "technical_support" = 0.10
+)
+CONTRACT_HRS <- c("Full-Time" = 0.80,
+                  "Part-Time" = 0.20)
+SCHOOL <- c(
+  "SEIT" = 0.40,
+  "HASS" = 0.20,
+  "BUS" = 0.20,
+  "SCI" = 0.20
+)
 
-par(mfrow = c(2, 2))
-plot(Staff_Weekly_Model)
+#- Calculate city student populations
+city_student_pop <-
+  enframe(x_prod_list(SCHOOL, CAREER),
+          name = "ID",
+          value = "Weight") %>%
+  mutate(n = Weight * N_STUDENT)
 
-#- outsample validation ----
-Data_Staff_Weekly_Train <- 
-  Data_Staff_Weekly %>% 
-  sample_frac(0.8)
+#- Calculate city staff populations
+city_staff_pop <-
+  enframe(x_prod_list(SCHOOL, CONTRACT_HRS),
+          name = "ID",
+          value = "Weight") %>%
+  mutate(n = Weight * N_STAFF)
 
-Data_Staff_Weekly_Test <- 
-  Data_Staff_Weekly %>% 
-  filter(!(ID %in% Data_Staff_Weekly_Train$ID))
+#- Calculate average trips per week day (cross-classification method)
+city_student_pop <- city_student_pop %>%
+  full_join(av_trips_student, by = "ID") %>%
+  mutate(TotalTrips = n * AverageTrips / 5)
+city_staff_pop <- city_staff_pop %>%
+  full_join(av_trips_staff, by = "ID") %>%
+  mutate(TotalTrips = n * AverageTrips / 5)
 
-Model_Staff <- lm(WeeklyTrips~School+Level+FulltimeParttime, data=Data_Staff_Weekly_Train)
-summary(Model_Staff)
+write.csv(city_student_pop, "output data/city_student_pop.csv")
+write.csv(city_staff_pop, "output data/city_staff_pop.csv")
 
-Predicted_trips <- predict(Model_Staff, newdata = Data_Staff_Weekly_Test)
+city_total_trips = sum(city_student_pop$TotalTrips, na.rm=TRUE) + sum(city_staff_pop$TotalTrips, na.rm=TRUE) #remove NAs
 
-Data_Staff_Weekly_Test <- 
-  Data_Staff_Weekly_Test %>% 
-  cbind(Predicted_trips)
+# TODO: need to factor in that the av. weekly trips is counting weekends 
+
+#----------------------------------------------#
+#--- 2. TRIP GENERATION -----------------------#
+#----------------------------------------------#
+
+#Details for the City Campus
+Data_Zones <- data.frame(
+  Zone = c(
+    "Belconnen",
+    "Gungahlin",
+    "Inner North",
+    "Inner South",
+    "Woden",
+    "West Creek",
+    "Tuggeranong",
+    "Queanbeyan"
+  ),
+  Population = c(100040, 83167, 58702, 27618, 34551, 20611, 82649, 61031),
+  Distance_City = c(13.30, 14.91, 5.70, 6.00, 11.00, 15.90, 19.40, 14.70),
+  Auto_TT_mean = c(17, 31, 12, 12, 15, 23, 29, 25),
+  PT_TT_min = c(31, 51, 23, 19, 36, 32, 48, 34),
+  PT_TT_max = c(38, 71, 34, 33, 56, 47, 72, 49),
+  PT_TT_mean = c(33, 62, 29, 27, 49, 42, 64, 44),
+  PT_Cost_Student = 1.66,
+  PT_Cost_Staff = 3.22
+)
+
+#---- Gravity ----
+Data_Zones <-
+  Data_Zones %>%
+  mutate(
+    # This tutorial uses distance as the generalised cost
+    FrictionFactor = Distance_City ^ (-0.41) / 0.41,
+    # This tutorial assumes alpha to be 0.1
+    Alpha = 2.5514E-6,
+    # The gravity model:
+    Trips = Alpha * Population * city_total_trips * FrictionFactor
+  )
+
+#---- Visualisation ----
+print(Data_Zones %>% select(Zone, Trips))
+
+Data_Zones %>%
+  ggplot() +
+  geom_col(aes(x = reorder(Zone,-Trips), y = Trips)) +
+  labs(x = "Zone") +
+  theme_bw()
+
+Data_Zones %>%
+  select(Zone, Trips, Distance_City, Population) %>%
+  mutate(Zone = reorder(Zone,-Trips)) %>%
+  gather(-Zone, key = Measure, value = Value) %>%
+  ggplot() +
+  geom_col(aes(x = Zone, y = Value)) +
+  facet_grid(rows = vars(Measure), scales = "free") +
+  theme_bw()
+
+Data_Zones %>%
+  ggplot() +
+  geom_col(aes(
+    x = reorder(Zone,-Trips),
+    y = Trips,
+    fill = Population
+  )) +
+  labs(x = "Zone") +
+  theme_bw()
 
 
-Data_Staff_Weekly_Test %>% 
-  ggplot()+
-  geom_point(aes(x=Predicted_trips, y=WeeklyTrips), position = "jitter")
+#- calibration debug
+#Total trip production:
+Data_Zones$Trips %>% sum()
+#Total trip attraction
+city_total_trips
+
+#----------------------------------------------#
+#--- 3. Mode Choice ---------------------------#
+#----------------------------------------------#
+
+Auto_Cost_KM = 0.70 # 35c/km charge
 
 
-Data_Staff_Weekly_Test %>% 
-  mutate(Difference = (Predicted_trips - WeeklyTrips)^2) %>% 
-  group_by() %>% 
-  summarise(RMSE = (sum(Difference)/nrow(Data_Staff_Weekly_Test))^0.5)
+#---- Utilities ----
+Data_Zones <-
+  Data_Zones %>%
+  mutate(
+    PT_Reliability = (PT_TT_max - PT_TT_min) / PT_TT_mean,
+    Auto_Cost = Auto_Cost_KM * Distance_City
+  ) %>%
+  #Assuming three modes are available
+  #Calculating utilities for students
+  mutate(
+    U_Auto = -0.25 * Auto_TT_mean - 0.11 * Auto_Cost,
+    U_PT = -0.26 - 0.15 * PT_TT_mean - 0.13 * PT_Cost_Student - 0.01 *
+      PT_Reliability,
+    U_Active = 0.1 - 0.67 * Distance_City
+  ) 
+
+#---- Probabilities  ----
+Data_Zones <-  Data_Zones %>%
+  mutate(
+    Exp_U_Auto = exp(U_Auto),
+    Exp_U_PT = exp(U_PT),
+    Exp_U_Active = exp(U_Active),
+    Exp_Sum = Exp_U_Auto + Exp_U_PT + Exp_U_Active,
+    
+    Prob_Auto = Exp_U_Auto / Exp_Sum,
+    Prob_PT = Exp_U_PT / Exp_Sum,
+    Prob_Active = Exp_U_Active / Exp_Sum,
+
+    Trips_Auto = Trips * Prob_Auto,
+    Trips_PT = Trips * Prob_PT,
+    Trips_Active = Trips * Prob_Active
+  )
+
+Data_Zones %>%
+  select(Zone, contains("Prob")) %>%
+  mutate(Zone = reorder(Zone,-Prob_Auto)) %>%
+  gather(-Zone, key = Mode, value = Probability) %>%
+  mutate(Mode = ordered(Mode, levels = c("Prob_Active", "Prob_PT", "Prob_Auto"))) %>%
+  ggplot() +
+  geom_col(aes(x = Zone, y = Probability, fill = Mode)) +
+  theme_bw()
 
 
+write.csv(Data_Zones %>% adorn_totals(name = 'TOTAL') %>% select(Zone, contains("Trips")), "output data/predicted_trips_by_zone_mode.csv")
 
+print(Data_Zones %>% adorn_totals(name = 'TOTAL') %>% select(Zone, contains("Trips")))
 
+#---- Visualisation  ----
 
-
-
-
-#- Random visualisation ----
-# Data_Student_Weekly %>% 
-#   group_by(School) %>% 
-#   summarise(`Mean trips` = mean(WeeklyTrips)) %>% 
-#   ggplot()+
-#   geom_col(aes(x=School, y=`Mean trips`)) +
-#   geom_text(aes(x=School, y=`Mean trips`,label=round(`Mean trips`, digits=2)), vjust=1.6, color="white",
-#             position = position_dodge(0.5), size=3.5) +
-#   ggtitle("Student weekly mean (trips) by school - ADFA sample")
-# 
-# Days_by_School <- Data_Student %>% 
-#   group_by( Day, School) %>% 
-#   summarise(trips = n()) %>% 
-#   group_by( Day, School) %>% 
-#   summarise(`Trips` = sum(trips))
-# 
-# Days_by_School$Day <- factor(Days_by_School$Day, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")) 
-#   
-# Days_by_School %>% 
-#   ggplot() +
-#   geom_col(aes(x=Day, y=`Trips`, fill=School)) +
-#   facet_wrap(~School) +
-#   ggtitle("School number of student trips by day - ADFA sample")
