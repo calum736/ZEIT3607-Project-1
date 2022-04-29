42e1ae10adb7ad2adc6c284f23e22db2753fd21e
calum736 <36124506+calum736@users.noreply.github.com>
2022-04-29 10:59:34 +1000

    Updated to separate student and staff when calculating trip dist and zone choice

--- Data Analysis v2.R
+++ Data Analysis v2.R
@@ -1,5 +1,6 @@
 
 
+
 #---- PACKAGES ----
 library(tidyverse)
 library(janitor)
@@ -22,6 +23,8 @@ x_prod_list <- function(A, B) {
 #----------------------------------------------#
 #--- LOAD SAMPLE DATA -------------------------#
 #----------------------------------------------#
+
+setwd("~/ZEIT3607/Project 1")
 
 #- Read student and staff data from master file
 student_master <- read.csv(file = "data/1_Students.csv")
@@ -119,12 +122,10 @@ city_total_trips = sum(city_student_pop$TotalTrips, na.rm = TRUE) + sum(city_sta
 
 # TODO: need to factor in that the av. weekly trips is counting weekends
 
-#----------------------------------------------#
-#--- 2. TRIP GENERATION -----------------------#
-#----------------------------------------------#
+
 
 #Details for the City Campus
-Data_Zones <- data.frame(
+city_zone_data <- data.frame(
   Zone = c(
     "Belconnen",
     "Gungahlin",
@@ -140,112 +141,134 @@ Data_Zones <- data.frame(
   Auto_TT_mean = c(17, 31, 12, 12, 15, 23, 29, 25),
   PT_TT_min = c(31, 51, 23, 19, 36, 32, 48, 34),
   PT_TT_max = c(38, 71, 34, 33, 56, 47, 72, 49),
-  PT_TT_mean = c(33, 62, 29, 27, 49, 42, 64, 44),
-  PT_Cost_Student = 1.66,
-  PT_Cost_Staff = 3.22
+  PT_TT_mean = c(33, 62, 29, 27, 49, 42, 64, 44)
 )
 
-#---- Gravity ----
-Data_Zones <-
-  Data_Zones %>%
-  mutate(
-    # This tutorial uses distance as the generalised cost
-    FrictionFactor = Distance_City ^ (-0.41) / 0.41,
-    # This tutorial assumes alpha to be 0.1
-    Alpha = 2.5514E-6,
-    # The gravity model:
-    Trips = Alpha * Population * city_total_trips * FrictionFactor
-  )
-
-#---- Visualisation ----
-print(Data_Zones %>% select(Zone, Trips))
-
-Data_Zones %>%
-  ggplot() +
-  geom_col(aes(x = reorder(Zone, -Trips), y = Trips)) +
-  labs(x = "Zone") +
-  theme_bw()
-
-Data_Zones %>%
-  select(Zone, Trips, Distance_City, Population) %>%
-  mutate(Zone = reorder(Zone, -Trips)) %>%
-  gather(-Zone, key = Measure, value = Value) %>%
-  ggplot() +
-  geom_col(aes(x = Zone, y = Value)) +
-  facet_grid(rows = vars(Measure), scales = "free") +
-  theme_bw()
-
-Data_Zones %>%
-  ggplot() +
-  geom_col(aes(
-    x = reorder(Zone, -Trips),
-    y = Trips,
-    fill = Population
-  )) +
-  labs(x = "Zone") +
-  theme_bw()
-
-
-#- calibration debug
-#Total trip production:
-Data_Zones$Trips %>% sum()
-#Total trip attraction
-city_total_trips
-
-#----------------------------------------------#
-#--- 3. Mode Choice ---------------------------#
-#----------------------------------------------#
-
-Auto_Cost_KM = 0.70 # 35c/km charge
-
-
-#---- Utilities ----
-Data_Zones <-
-  Data_Zones %>%
-  mutate(
-    PT_Reliability = (PT_TT_max - PT_TT_min) / PT_TT_mean,
-    Auto_Cost = Auto_Cost_KM * Distance_City
-  ) %>%
-  #Assuming three modes are available
-  #Calculating utilities for students
-  mutate(
-    U_Auto = -0.25 * Auto_TT_mean - 0.11 * Auto_Cost,
-    U_PT = -0.26 - 0.15 * PT_TT_mean - 0.13 * PT_Cost_Student - 0.01 *
-      PT_Reliability,
-    U_Active = 0.1 - 0.67 * Distance_City
-  )
-
-#---- Probabilities  ----
-Data_Zones <-  Data_Zones %>%
-  mutate(
-    Exp_U_Auto = exp(U_Auto),
-    Exp_U_PT = exp(U_PT),
-    Exp_U_Active = exp(U_Active),
-    Exp_Sum = Exp_U_Auto + Exp_U_PT + Exp_U_Active,
+calc_zone_trips <-
+  function(zone_data,
+           total_trips,
+           Auto_Cost_KM,
+           PT_Cost) {
+    #----------------------------------------------#
+    #--- 2. TRIP DISTRIBUTION ---------------------#
+    #----------------------------------------------#
     
-    Prob_Auto = Exp_U_Auto / Exp_Sum,
-    Prob_PT = Exp_U_PT / Exp_Sum,
-    Prob_Active = Exp_U_Active / Exp_Sum,
+    #---- Gravity ----
+    zone_data <-
+      zone_data %>%
+      mutate(
+        # This tutorial uses distance as the generalised cost
+        FrictionFactor = Distance_City ^ (-0.41) / 0.41,
+        # This tutorial assumes alpha to be 0.1
+        Alpha = 2.5514E-6,
+        # The gravity model:
+        Trips = Alpha * Population * total_trips * FrictionFactor
+      )
     
-    Trips_Auto = Trips * Prob_Auto,
-    Trips_PT = Trips * Prob_PT,
-    Trips_Active = Trips * Prob_Active
-  )
+    #- calibration debug
+    #Total trip production:
+    zone_data$Trips %>% sum()
+    #Total trip attraction
+    city_total_trips
+    
+    #---- Visualisation ----
+    print(zone_data %>% select(Zone, Trips))
+    
+    zone_data %>%
+      ggplot() +
+      geom_col(aes(x = reorder(Zone,-Trips), y = Trips)) +
+      labs(x = "Zone") +
+      theme_bw()
+    
+    zone_data %>%
+      select(Zone, Trips, Distance_City, Population) %>%
+      mutate(Zone = reorder(Zone,-Trips)) %>%
+      gather(-Zone, key = Measure, value = Value) %>%
+      ggplot() +
+      geom_col(aes(x = Zone, y = Value)) +
+      facet_grid(rows = vars(Measure), scales = "free") +
+      theme_bw()
+    
+    zone_data %>%
+      ggplot() +
+      geom_col(aes(
+        x = reorder(Zone,-Trips),
+        y = Trips,
+        fill = Population
+      )) +
+      labs(x = "Zone") +
+      theme_bw()
+    
+    #----------------------------------------------#
+    #--- 3. Mode Choice ---------------------------#
+    #----------------------------------------------#
+    
+    #---- Utilities ----
+    zone_data <-
+      zone_data %>%
+      mutate(
+        PT_Reliability = (PT_TT_max - PT_TT_min) / PT_TT_mean,
+        Auto_Cost = Auto_Cost_KM * Distance_City
+      ) %>%
+      #Assuming three modes are available
+      #Calculating utilities for students
+      mutate(
+        U_Auto = -0.25 * Auto_TT_mean - 0.11 * Auto_Cost,
+        U_PT = -0.26 - 0.15 * PT_TT_mean - 0.13 * PT_Cost - 0.01 * PT_Reliability,
+        U_Active = 0.1 - 0.67 * Distance_City
+      )
+    
+    #---- Probabilities  ----
+    zone_data <-  zone_data %>%
+      mutate(
+        Exp_U_Auto = exp(U_Auto),
+        Exp_U_PT = exp(U_PT),
+        Exp_U_Active = exp(U_Active),
+        Exp_Sum = Exp_U_Auto + Exp_U_PT + Exp_U_Active,
+        
+        Prob_Auto = Exp_U_Auto / Exp_Sum,
+        Prob_PT = Exp_U_PT / Exp_Sum,
+        Prob_Active = Exp_U_Active / Exp_Sum,
+        
+        Trips_Auto = Trips * Prob_Auto,
+        Trips_PT = Trips * Prob_PT,
+        Trips_Active = Trips * Prob_Active
+      )
+    
+    #---- Visualisation  ----
+    zone_data %>%
+      select(Zone, contains("Prob")) %>%
+      mutate(Zone = reorder(Zone,-Prob_Auto)) %>%
+      gather(-Zone, key = Mode, value = Probability) %>%
+      mutate(Mode = ordered(Mode, levels = c("Prob_Active", "Prob_PT", "Prob_Auto"))) %>%
+      ggplot() +
+      geom_col(aes(x = Zone, y = Probability, fill = Mode)) +
+      theme_bw()
+    
+    return (zone_data)
+  }
 
-#---- Visualisation  ----
-Data_Zones %>%
-  select(Zone, contains("Prob")) %>%
-  mutate(Zone = reorder(Zone, -Prob_Auto)) %>%
-  gather(-Zone, key = Mode, value = Probability) %>%
-  mutate(Mode = ordered(Mode, levels = c("Prob_Active", "Prob_PT", "Prob_Auto"))) %>%
-  ggplot() +
-  geom_col(aes(x = Zone, y = Probability, fill = Mode)) +
-  theme_bw()
+Auto_Cost_KM = 0.35 * 2  # ATO $0.7/km * 2 since return trip
+
+
+zone_trips_student <- calc_zone_trips(city_zone_data,
+                                      sum(city_student_pop$TotalTrips, na.rm = TRUE),
+                                      Auto_Cost_KM,
+                                      1.66)
+zone_trips_staff <- calc_zone_trips(city_zone_data,
+                                   sum(city_staff_pop$TotalTrips, na.rm = TRUE),
+                                   Auto_Cost_KM,
+                                   3.22)
+zone_trips_total <- rbind(zone_trips_student, zone_trips_staff) %>% 
+  select(Zone, contains("Trips")) %>% 
+  group_by(Zone) %>% 
+  summarise_all(sum) %>% 
+  adorn_totals(name = 'TOTAL')
 
 #---- Write output to file and print to console  ----
 write.csv(
-  Data_Zones %>% adorn_totals(name = 'TOTAL') %>% select(Zone, contains("Trips")),
+  zone_trips_total,
   "output data/predicted_trips_by_zone_mode.csv"
 )
 
-print(Data_Zones %>% adorn_totals(name = 'TOTAL') %>% select(Zone, contains("Trips")))
+print(zone_trips_total)
